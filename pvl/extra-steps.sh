fef0bb2bb80867cafa1b60264b5ba54896e8717f
pexarkh <40366153+pexarkh@users.noreply.github.com>
2024-10-22 18:43:28 -0400

    -

---
+++ pvl/extra-steps.sh
@@ -0,0 +1,6 @@
+# to build a wheel...
+
+python3 -m pip install -r pvl/requirements3.txt
+python setup.py bdist_wheel
+
+#.whl: dist/flanker-0.9.15+preveil1-py3-none-any.whl
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ tests_require = [
 ],
 
 setup(name='flanker',
-      version='0.9.11',
+      version='0.9.15+preveil1',
       description='Mailgun Parsing Tools',
       long_description=open('README.rst').read(),
       classifiers=[
